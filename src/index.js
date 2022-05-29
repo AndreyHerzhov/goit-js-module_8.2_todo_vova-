42e1bba363a7af1da54305c86ffdd16694b87c12
AndreyHerzhov <andreyherzhov@gmail.com>
2022-05-29 11:33:10 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
  import { throttle } from "lodash";
  import "./styles/style.scss";
+ import { refs } from "./scripts/refs"
+ import { createLi, addLiToList } from "./scripts/generateMarkdown";
+import { storage } from "./helper/storage";
+
+
+
+ const handleMount = () => {
+     const list = localStorage.getItem('list')
+     try {
+         const savedList = JSON.parse(list)
+         const markdown = savedList.reduce((acc, text) => acc + createLi(text), '')
+         addLiToList(markdown)
+         console.log(savedList)
+     } catch (error) {
+         console.log('parsing error')
+     }
+ }
+
+ const handleSubmit = function (e) {
+     e.preventDefault()
+     const value = refs.input.value
+     const liItem = createLi(value)
+     addLiToList(liItem)
+     const result = storage.readItem('list',[])
+     result.push(value)
+     storage.addItem("list",result)
+     console.log(result)
+        // const list = localStorage.getItem('list')
+        // try {
+        //     const listData = list ? JSON.parse(list) : []
+        //     listData.push(value)
+        //     const updatedtList = JSON.stringify(listData)
+        //     localStorage.setItem('list', updatedtList)
+        // } catch (error) {
+        //     console.log('parsing error')
+        // }
+       
+     refs.input.value = ''
+ }
+
+
+
+refs.form.addEventListener('submit', handleSubmit)
+addEventListener('DOMContentLoaded', handleMount)
  
 
- console.log(throttle)
  
-
- console.log('Hallo')
---
+++ src/scripts/refs.js
@@ -0,0 +1,9 @@
+export const refs = {
+    form: document.querySelector('form'),
+    input: document.querySelector('input'),
+    button: document.querySelector('button'),
+    list: document.querySelector('.list')
+}
+
+ 
+
